2ff9f330de99db173bda2784ddf250622a4bbc26
wuhaidong <charleskinx@outlook.com>
2023-03-20 21:45:54 +0800

    EditText和Button控件的使用

--- app/src/main/java/com/example/studyandroid/MainActivity.kt
+++ app/src/main/java/com/example/studyandroid/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.studyandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.example.studyandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        val editText = binding.editTest
+        val button = binding.buttonTest
+
+        setContentView(binding.root)
+        button.setOnClickListener {
+            val message = editText.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
